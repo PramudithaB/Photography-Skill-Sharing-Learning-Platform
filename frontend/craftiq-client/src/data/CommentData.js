f2264a2c8c532af0620b92a83629359ec75bd641
Shehan726 <158991760+Shehan726@users.noreply.github.com>
2025-05-07 23:10:17 +0530

    changes

--- frontend/craftiq-client/src/data/CommentData.js
+++ frontend/craftiq-client/src/data/CommentData.js
@@ -8,28 +8,28 @@ const CommentData = [
       id: 1,
       img: img1,
       userId: 1,
-      pargrhap: "Multiply sea night grass fourth day sea lesser rule open subdue female fill which them Blessed, give fill lesser bearing multiply sea night grass fourth day sea lesser",
-      author: "Shehan",
+      pargrhap: "Grate Picture",
+      author: "Shehan Menaka",
       date: "May 5, 2025 at 3:12 pm",
-      //  postTitle: "Second divided from form fish beast made every of seas all gathered us saying he our"
+      postTitle: "Morning View"
     },
     {
       id: 2,
       img: img2,
       userId: 2,
-      pargrhap: "Lesser air gathering heaven second creature let itself above dry stars tree gathered they're second created",
-      author: "John Doe",
-      date: "January 10, 2018 at 1:45 pm",
-       postTitle: "Second divided from form fish beast made every of seas all gathered us saying he our"
+      pargrhap: "wooooooow!",
+      author: "Ravindu Yasas",
+      date: "May 4, 2025 at 5:12 pm",
+      postTitle: "My New Car"
     },
     {
       id: 3,
       img: img3,
       userId: 1,
-      pargrhap: "Fruitful gathering waters. Subdue make saw seed without image fly their. Greater man saying.",
-      author: "Jane Smith",
-      date: "March 22, 2019 at 6:30 am",
-      postTitle: "Second divided from form fish beast made every of seas all gathered us saying he our"
+      pargrhap: "wooooooow!",
+      author: "Ravindu Yasas",
+      date: "May 4, 2025 at 5:12 pm",
+      postTitle: "My New Car"
     }
   ];
   
