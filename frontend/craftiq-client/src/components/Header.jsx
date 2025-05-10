b6c10e802925c426f0c5c46da539927b152d2385
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2025-05-10 09:02:51 +0530

    .

--- frontend/craftiq-client/src/components/Header.jsx
+++ frontend/craftiq-client/src/components/Header.jsx
@@ -12,42 +12,8 @@ function Header() {
         {/* Header Start */}
         <div className="header-area">
           <div className="main-header ">
-            <div className="header-top black-bg d-none d-sm-block">
-              <div className="container">
-                <div className="col-xl-12">
-                  <div className="row d-flex justify-content-between align-items-center">
-                    <div className="header-info-left">
-                      <ul>
-                        <li className="title"><span className="flaticon-energy" /> trending-title</li>
-                        <li>Class property employ ancho red multi level mansion</li>
-                      </ul>
-                    </div>
-                    <div className="header-info-right">
-                      <ul className="header-date">
-                        <li><span className="flaticon-calendar" /> +880166 253 232</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="header-mid gray-bg">
-              <div className="container">
-                <div className="row d-flex align-items-center">
-                  {/* Logo */}
-                  <div className="col-xl-3 col-lg-3 col-md-3 d-none d-md-block">
-                    <div className="logo">
-                      <Link to="/user/home"><img src={logo} alt="" /></Link> {/* Use Link for navigation */}
-                    </div>
-                  </div>
-                  <div className="col-xl-9 col-lg-9 col-md-9">
-                    <div className="header-banner f-right ">
-                      <img src="assets/img/gallery/header_card.png" alt="" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            
+         
             <div className="header-bottom header-sticky">
               <div className="container">
                 <div className="row align-items-center">
